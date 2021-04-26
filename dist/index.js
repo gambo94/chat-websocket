63a743b4555ad78ba0f06715220ecf5bb9f437d2
gambo94 <fedigambo94@gmail.com>
2021-04-26 19:54:43 +0200

    uploading socket implementation

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,28 @@
+var path = require('path');
 var express = require('express');
-var app = express();
+var http = require('http');
 var routes = require('./routes/router');
 var morgan = require('morgan');
+var app = express();
+// Setting up socket
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+// event 
+io.on('connection', function (socket) {
+    console.log('new user connected');
+    socket.emit('message', 'Welcome to the chattt');
+});
 // Settings
 app.set('port', process.env.PORT || 3000);
 // middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+// static files
+var parentDir = path.dirname(__dirname);
+app.use(express.static(path.join(parentDir, 'public')));
 // routes
 app.use(routes);
-app.listen(app.get('port'), function () {
+server.listen(app.get('port'), function () {
     // connecting to DB
     var connect = require('./config/dbConnect')();
     // server running
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
+const path = require('path');
 const express = require('express');
-const app = express();
+const http = require('http');
 const routes = require('./routes/router')
 const morgan = require('morgan');
 
+
+const app = express();
+
+// Setting up socket
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+
+// event 
+io.on('connection', socket => {
+    console.log('new user connected');
+
+    socket.emit('message', 'Welcome to the chattt')
+});
+
 // Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
 
 // middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+// static files
+const parentDir = path.dirname(__dirname);
+app.use(express.static(path.join(parentDir, 'public')));
 
 // routes
 app.use(routes);
@@ -17,7 +37,7 @@ app.use(routes);
 
 
 
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
     // connecting to DB
     const connect = require('./config/dbConnect')();
     // server running
